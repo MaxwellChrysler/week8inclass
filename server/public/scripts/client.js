d5607c71e63024a340509cad5fe2aa9dbecaa51d
Maxwell Chrysler <maxwellchrysler@hotmail.com>
2023-05-15 13:50:31 -0500

    create joke object

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,3 +5,14 @@ $( document ).ready( onReady );
 function onReady() {
     console.log('DOM ready');
 }
+
+let jokes = {
+
+
+    whoseJoke: $("whoseJokeIn").val(),
+    jokeQuestion:$("questionIn").val() ,
+    punchLine:$("punchlineIn").val()
+}
+
+
+console.log('Jokes',jokes)
