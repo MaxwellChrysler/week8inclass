c86b6e2df9098945ff3490e7fffa78b50f358f73
Maxwell Chrysler <maxwellchrysler@hotmail.com>
2023-05-15 14:44:41 -0500

    2:45

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,15 +4,78 @@ $( document ).ready( onReady );
 
 function onReady() {
     console.log('DOM ready');
+// add on click
+$('#addJokeButton').on('click',sendToServer);
+
+getJokeList(); 
 }
 
-let jokes = {
 
+
+function sendToServer(event){
+    event.preventDefault;
+
+    let jokeObject = { // put together the jokes in the format
 
     whoseJoke: $("whoseJokeIn").val(),
     jokeQuestion:$("questionIn").val() ,
     punchLine:$("punchlineIn").val()
-}
+    }; // end inputing object
+    //ajax post
+
+    $.ajax({
+        method: 'POST',
+        url:'/jokeObject',
+        
+    }).then(
+            function(response){
+            getJokeList()
+    }
+    ).catch(
+        function(error){
+            console.log('POST /jokeObject call failed');
+            console.log('error',error)
+        }
+    )
+
+} // end function
+
+function getJokeList(){ // getEquationsAndAddToPage
+    $.ajax({
+        method: 'GET',
+        url: '/jokeObject'
+    }).then(
+        function(response){
 
 
-console.log('Jokes',jokes)
+
+
+        }
+
+        //
+//ajax GET 
+// in the array and the append
+
+)
+
+
+
+} // end of getJokeList
+console.log('Jokes',jokeObject);
+
+
+
+
+// function sendJokeList(event){
+//     event.preventDefault();
+
+//     whoseJoke
+
+
+// }
+//ajax POST
+
+//app.get
+
+
+//app.post
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const PORT = 5000;
 // use bodyParser.urlencoded throughout the app with this:
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let jokes = [
+let jokes = [ //jokes array no need to do anything
   {
     whoseJoke: "Danny",
     jokeQuestion: "Why do scuba divers fall backwards out of boats?",
@@ -33,6 +33,33 @@ let jokes = [
     punchLine: "It was a shih tzu."
   }
 ];
+
+
+app.get('/jokeObject', (req, res) => { // 
+  res.send(jokes); // go get the array 
+})
+
+app.post('/jokeObject'),(req, res) => {
+  let whoseJoke = (req.body.whoseJoke);
+  let jokeQuestion = (req.body.jokeQuestion);
+  let punchLine = (req.body.punchLine);
+
+  let jokeObject = {
+      whoseJoke: whoseJoke ,
+      jokeQuestion: jokeQuestion,
+      punchLine: punchLine
+
+  }
+
+
+
+    jokes.push(jokeObject); // put the new jokes into the joke array
+
+    res.sendStatus(201); // completed 
+}
+
+
+
 
 // serve back static files
 app.use(express.static('server/public'));
